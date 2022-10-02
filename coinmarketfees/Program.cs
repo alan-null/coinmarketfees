3a3658e44537cd148a90e0b1bd754cf495a8bd4f
Alan Płócieniak <alan-null@outlook.com>
2022-10-02 13:08:02 +0200

    Added progress bar

--- coinmarketfees/Program.cs
+++ coinmarketfees/Program.cs
@@ -41,8 +41,14 @@ namespace coinmarketfees
             {
                 case "getexchanges":
                     {
-                        var exchanges = htmlParser.GetExchanges(htmlClient.GetExchanges()).OrderBy(k => k.Name).ToList();
-                        exchanges.ForEach(e => Console.WriteLine($"{e.Name} [{e.Symbol}]"));
+                        using (var progress = new ProgressBar())
+                        {
+                            progress.Report(0);
+                            var exchanges = htmlParser.GetExchanges(htmlClient.GetExchanges()).OrderBy(k => k.Name).ToList();
+                            progress.Dispose();
+                            Console.WriteLine($"Exchanges");
+                            exchanges.ForEach(e => Console.WriteLine($"{e.Name} [{e.Symbol}]"));
+                        }
                         break;
                     }
                 case "getfees":
@@ -53,28 +59,47 @@ namespace coinmarketfees
                         }
                         if (!string.IsNullOrEmpty(options.Exchange2))
                         {
-                            var e1 = htmlClient.GetExchange(options.Exchange);
-                            var e2 = htmlClient.GetExchange(options.Exchange2);
-                            var exchange1 = htmlParser.GetExchange(e1);
-                            var exchange2 = htmlParser.GetExchange(e2);
-
-                            var fiKraken = GetFeesInfo(exchange1.Id);
-                            var fiBinance = GetFeesInfo(exchange2.Id);
-
-                            var common = fiKraken.Where(bc => fiBinance.Any(cc => cc.CoinSymbol == bc.CoinSymbol));
-                            common = common.Where(k => k.Price > 0).OrderBy(k => k.Price);
-                            foreach (var e in common)
+                            using (var progress = new ProgressBar())
                             {
-                                Console.WriteLine($"{e.CoinName} [{e.CoinSymbol}][{e.Price}]");
+                                progress.Report(0);
+                                var e1 = htmlClient.GetExchange(options.Exchange);
+                                progress.Report(0.1);
+                                var e2 = htmlClient.GetExchange(options.Exchange2);
+                                progress.Report(0.2);
+                                var exchange1 = htmlParser.GetExchange(e1);
+                                var exchange2 = htmlParser.GetExchange(e2);
+
+                                var fiKraken = GetFeesInfo(exchange1.Id);
+                                progress.Report(0.5);
+                                var fiBinance = GetFeesInfo(exchange2.Id);
+                                progress.Report(0.8);
+
+                                var common = fiKraken.Where(bc => fiBinance.Any(cc => cc.CoinSymbol == bc.CoinSymbol));
+                                common = common.Where(k => k.Price > 0).OrderBy(k => k.Price);
+                                progress.Report(1);
+                                progress.Dispose();
+                                Console.WriteLine($"Transfer fees for exchange: {exchange1.Name} (target: {exchange2.Name})");
+                                foreach (var e in common)
+                                {
+                                    Console.WriteLine($"{e.CoinName} [{e.CoinSymbol}][{e.Price}]");
+                                }
                             }
                             return;
                         }
                         else
                         {
-                            var kraken = htmlClient.GetExchange(options.Exchange);
-                            var exchange = htmlParser.GetExchange(kraken);
-                            var exchanges = GetFeesInfo(exchange.Id);
-                            exchanges.ForEach(e => Console.WriteLine($"{e.CoinName} [{e.CoinSymbol}][{e.Price}]"));
+                            using (var progress = new ProgressBar())
+                            {
+                                progress.Report(0);
+                                var kraken = htmlClient.GetExchange(options.Exchange);
+                                progress.Report(0.2);
+                                var exchange = htmlParser.GetExchange(kraken);
+                                var exchanges = GetFeesInfo(exchange.Id);
+                                progress.Report(1);
+                                progress.Dispose();
+                                Console.WriteLine($"Transfer fees for exchange: {exchange.Name}");
+                                exchanges.ForEach(e => Console.WriteLine($"{e.CoinName} [{e.CoinSymbol}][{e.Price}]"));
+                            }
                             break;
                         }
                     }
