5131561877d5cf7f224011f9b27ed102a08f4000
Alan Płócieniak <alan-null@outlook.com>
2022-10-02 12:42:26 +0200

    Added CLI

---
+++ coinmarketfees/HtmlClient.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace coinmarketfees
+{
+    public class HtmlClient
+    {
+        protected const string _baseUrl = "https://coinmarketfees.com";
+        protected readonly RestClient client = new RestClient();
+
+        protected virtual string FetchHtml(string url)
+        {
+            var client = new RestClient();
+            var r = new RestRequest(url, Method.Get);
+            var response = client.Execute(r);
+            var html = response.Content;
+            return html;
+        }
+
+        public virtual string GetExchange(string name)
+        {
+            string url = $"{_baseUrl}/exchange/{name}";
+            return FetchHtml(url);
+        }
+
+        public virtual string GetExchanges()
+        {
+            string url = $"{_baseUrl}/exchanges";
+            return FetchHtml(url);
+        }
+
+        public virtual string GetFeesInfo(int exchange, int page = 1)
+        {
+            string url = $"{_baseUrl}/search-all.php?action=exchange_search_coin&exchange={exchange}&dataPage={page}";
+            var html = FetchHtml(url);
+            var dyna = JsonConvert.DeserializeObject(html);
+            html = (dyna as dynamic).html;
+            return html;
+        }
+    }
+}
---
+++ coinmarketfees/HtmlParser.cs
@@ -0,0 +1,73 @@
+﻿using coinmarketfees.Model;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace coinmarketfees
+{
+    public class HtmlParser
+    {
+        public Exchange GetExchange(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var node = doc.DocumentNode.SelectNodes("//input").Where(e => e.GetAttributeValue("name", string.Empty) == "exchange").FirstOrDefault();
+            var selector = doc.DocumentNode.SelectNodes("//select").Where(e => e.HasClass("comparison_select")).FirstOrDefault()?
+                .SelectNodes(".//option").Where(a => a.Attributes["selected"] != null).FirstOrDefault();
+            if (node != null && selector != null)
+            {
+                return new Exchange
+                {
+                    Id = int.Parse(node.GetAttributeValue("value", "0")),
+                    Symbol = selector.Attributes["value"].Value,
+                    Name = selector.InnerText
+                };
+            }
+            return null;
+        }
+
+        public List<Exchange> GetExchanges(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var nodes = doc.DocumentNode.SelectNodes("//div").Where(e => e.HasClass("info_text")).ToList();
+            var exchanges = new List<Exchange>();
+            foreach (var item in nodes)
+            {
+                var linkNode = item.SelectSingleNode(".//a");
+                string url = linkNode.Attributes["href"].Value;
+                var ex = new Exchange
+                {
+                    Name = linkNode.InnerText,
+                    Symbol = url.Substring(url.LastIndexOf('/') + 1)
+                };
+                exchanges.Add(ex);
+            }
+            return exchanges;
+        }
+
+        public List<FeeInfo> GetFeeInfo(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var nodes = doc.DocumentNode.SelectNodes("//div").Where(e => e.HasClass("item_cSearch") && e.HasClass("item_coin_network")).ToList();
+            List<FeeInfo> feeInfos = new List<FeeInfo>();
+            foreach (var item in nodes)
+            {
+                var symbol = item.Attributes["name"].Value;
+                var name = item.SelectSingleNode(".//h1[contains(@class, 'ctitle')]").InnerText;
+                var fee = item.SelectSingleNode(".//div[contains(@class, 'ttop network-fee')]").InnerText.TrimStart('$');
+
+                var fi = new FeeInfo
+                {
+                    CoinSymbol = symbol,
+                    CoinName = name,
+                    Price = fee == "N/A" ? 0 : decimal.Parse(fee)
+                };
+                feeInfos.Add(fi);
+            }
+            return feeInfos;
+        }
+    }
+}
